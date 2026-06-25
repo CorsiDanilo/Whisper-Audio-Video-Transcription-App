945f63f08aaa9cb0699867b8701df3ae6cc2f40e
DaniloCorsi <danilocorsi1997@gmail.com>
2026-06-25 14:15:31 +0200

    feat: enable browser launch and add multi-language support configuration file

--- app_main.py
+++ app_main.py
@@ -1,51 +1,3 @@
-"""
-Source: https://github.com/whitphx/gradio-pyinstaller-example
-
-To generate .exe file: 
-pyinstaller app_main.py --collect-data gradio --collect-data gradio_client --additional-hooks-dir=./hooks --runtime-hook ./runtime_hook.py --add-data --noconfirm --icon=logo.ico --distpath=./dist/whisper_with_cmd --name=Whisper
-pyinstaller app_main.py --collect-data gradio --collect-data gradio_client --additional-hooks-dir=./hooks --runtime-hook ./runtime_hook.py --noconsole --noconfirm --icon=logo.ico --distpath=./dist/whisper_no_cmd --name=Whisper
-
-Or simply run the following command:
-pyinstaller --noconfirm whisper.spec
-
-Troubleshooting:
-1 - PyInstaller cannot check for assembly dependencies
-    Solution:
-        Source: https://stackoverflow.com/questions/59638691/pyinstaller-cannot-check-for-assembly-dependencies
-        Navigate to your python directory -> Lib -> site-packages -> Pyinstaller. Open compat.py and look for the following:
-
-            if is_win:
-                try:
-                    from win32ctypes.pywin32 import pywintypes  # noqa: F401
-                    from win32ctypes.pywin32 import win32api
-                except ImportError:
-                    # This environment variable is set by setup.py
-                    # - It's not an error for pywin32 to not be installed at that point
-                    if not os.environ.get('PYINSTALLER_NO_PYWIN32_FAILURE'):
-                        raise SystemExit('PyInstaller cannot check for assembly dependencies.\n'
-                                    'Please install pywin32-ctypes.\n\n'
-                                    'pip install pywin32-ctypes\n')
-
-        Change both of those import statements to import the modules themselves instead of trying to grab them from win32ctypes.pywin32.
-
-            #from win32ctypes.pywin32 import pywintypes  # noqa: F401
-            #from win32ctypes.pywin32 import win32api
-            import pywintypes
-            import win32api
-
-        And install:
-        
-            pip install pypiwin32
-
-2 - FileNotFoundError: [Errno 2] No such file or directory: '...\\app\\_internal\\safehttpx\\version.txt'
-    Solution:
-        Navigate to your python directory -> Lib -> site-packages, copy the safehttpx folder and paste it into your python directory -> dist -> app -> _internal
-
-3 - failed:Load model 'C:...\\app\\_internal\\faster_whisper\\assets\\silero_encoder_v5.onnx' failed. File doesn't exist
-    Solution:
-        Navigate to your python directory -> Lib -> site-packages, copy the faster_whisper folder and paste it into your python directory -> dist -> app -> _internal
-"""
-
 import webview
 import threading
 import sys
@@ -53,6 +5,7 @@ import os
 import pystray
 from PIL import Image
 
+from config import get_translation as _
 from main import demo as main 
 from security_utils import get_gradio_launch_kwargs
 
@@ -80,8 +33,8 @@ def setup_tray():
         image = Image.new('RGB', (64, 64), color = 'white')
         
     menu = pystray.Menu(
-        pystray.MenuItem('Show / Hide App', on_show),
-        pystray.MenuItem('Exit', on_quit)
+        pystray.MenuItem(_('tray_show_hide'), on_show),
+        pystray.MenuItem(_('tray_exit'), on_quit)
     )
     
     icon = pystray.Icon("WhisperApp", image, "Whisper Audio/Video Utility", menu)
@@ -90,7 +43,6 @@ def setup_tray():
 from ui import custom_css
 
 main.launch(css=custom_css, **get_gradio_launch_kwargs(prevent_thread_lock=True))
-
 tray_thread = threading.Thread(target=setup_tray, daemon=True)
 tray_thread.start()
 
--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from ui import demo, custom_css
 from security_utils import get_gradio_launch_kwargs
 
 if __name__ == "__main__":
-    demo.launch(css=custom_css, **get_gradio_launch_kwargs(debug=True))
+    demo.launch(css=custom_css, inbrowser=True, **get_gradio_launch_kwargs(debug=True))
